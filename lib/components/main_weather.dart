1f3a6f5505cc907eb774792a1fb05dabbeaa586a
Dheeraj883 <deerajmishra883@gmail.com>
2022-04-13 14:05:34 +0530

    first commit

---
+++ lib/components/main_weather.dart
@@ -0,0 +1,72 @@
+import 'package:clima/components/weather_text.dart';
+import 'package:clima/utilities/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class MainWeatherView extends StatelessWidget {
+  const MainWeatherView({
+    Key? key,
+    required this.icon,
+    required this.description,
+    required this.temp,
+    required this.day,
+    required this.screenWidth,
+  }) : super(key: key);
+
+  final String icon;
+  final String description;
+  final String temp;
+  final String day;
+  final double screenWidth;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Lottie.asset(
+          'assets/$icon',
+          width: (screenWidth - ((screenWidth / 100) * 40)),
+          height: 200,
+        ),
+        Text(
+          description,
+          style: kLargeTitleTextStyle,
+        ),
+        const SizedBox(
+          height: 4.0,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              '°',
+              style: kTemperatureAlphaTextStyle,
+            ),
+            Text(
+              temp,
+              style: kTemperatureTextStyle,
+            ),
+            const Text(
+              '°',
+              style: kTemperatureTextStyle,
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 4.0,
+        ),
+        Text(
+          day,
+          style: kHeadlineTextStyle,
+        ),
+        const SizedBox(
+          height: 12.0,
+        ),
+        Text(
+          getMessage(int.parse(temp)),
+          style: kHeadlineTextStyle,
+        )
+      ],
+    );
+  }
+}
